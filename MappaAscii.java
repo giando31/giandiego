1e36fdfb4b1320a688bf19873e271b1f25668c95
giando31 <giando31@gmail.com>
2015-11-10 13:00:14 +0100

    Create MappaAscii.java

---
+++ MappaAscii.java
@@ -0,0 +1,28 @@
+public class MappaAscii{
+	public static void main(String[] args){
+		            System.out.println("                                    _______________                                 ");
+		            System.out.println("                                   |               |                                ");
+                System.out.println("                                   |               |                                ");
+            		System.out.println("                                   |piazza maggiore|                                ");
+                System.out.println("                                   |               |                                ");
+                System.out.println("                                   |               |                                ");
+                System.out.println("2torri=====================================================================pratello ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println(" | |                                       | |                                      ");
+                System.out.println("_| |___                                    | |                                      ");
+                System.out.println("piazza |                                   | |                                      ");
+                System.out.println("verdi  |                                   | |                                      ");                         
+                System.out.println("_______|                                   | |                                      ");
+                System.out.println("                                           | |                                      ");
+                System.out.println("                                           | |                                      ");
+                System.out.println("                                           | |                                      ");
+                System.out.println("                                           | |                                      ");
+                System.out.println("                                    stazione centrale                               ");
+}
+}
+
+
